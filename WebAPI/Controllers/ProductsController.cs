22ba18d470a05a9951380871e6ec87a8011bdb80
EdaKevserDemir <e.kevserdemir@gmail.com>
2021-08-14 01:30:01 +0300

    WebAPIStart

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,26 @@
+﻿using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public List<Product> Get()
+        {
+            return new List<Product>
+            {
+                new Product{ProductId=1,ProductName="Elma"},
+                new Product{ProductId=2,ProductName="Armut"}
+            };
+            
+        }
+    }
+}
